99506fcd123069af148274bd0c61c801a6839c79
Mehran Dashti <mehran19.md@gmail.com>
2026-06-07 21:01:43 +0330

    feat: Implement baser of mysql connection

---
+++ internal/database/database.go
@@ -0,0 +1,87 @@
+package database
+
+import (
+	"sync"
+
+	_ "github.com/go-sql-driver/mysql"
+	gormmysql "gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var (
+	db   *gorm.DB
+	once sync.Once
+)
+
+func Connect(dsn string) *gorm.DB {
+	once.Do(func() {
+		gormDB, err := gorm.Open(gormmysql.Open(dsn), &gorm.Config{
+			Logger: logger.Default.LogMode(logger.Silent),
+		})
+		if err != nil {
+			panic("failed to connect to database: " + err.Error())
+		}
+
+		// if err := applyPoolSettings(gormDB); err != nil {
+		// 	panic("failed to configure connection pool: " + err.Error())
+		// }
+
+		// if err := runMigrations(gormDB); err != nil {
+		// 	panic("failed to run migrations: " + err.Error())
+		// }
+
+		db = gormDB
+	})
+	return db
+}
+
+// func runMigrations(db *gorm.DB) error {
+// 	sqlDB, err := db.DB()
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	driver, err := mysql.WithInstance(sqlDB, &mysql.Config{})
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	// Use embedded FS — no disk access needed
+// 	sourceDriver, err := iofs.New(migrations.FS, ".")
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	m, err := migrate.NewWithInstance(
+// 		"iofs",
+// 		sourceDriver,
+// 		"mysql",
+// 		driver,
+// 	)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+// 		return err
+// 	}
+
+// 	version, _, _ := m.Version()
+// 	slog.Info("migrations applied", "version", version)
+// 	return nil
+// }
+
+// func applyPoolSettings(db *gorm.DB) error {
+// 	sqlDB, err := db.DB()
+// 	if err != nil {
+// 		return fmt.Errorf("get underlying sql.DB: %w", err)
+// 	}
+
+// 	sqlDB.SetMaxOpenConns(25)
+// 	sqlDB.SetMaxIdleConns(10)
+// 	sqlDB.SetConnMaxLifetime(5 * time.Minute)
+// 	sqlDB.SetConnMaxIdleTime(10 * time.Minute)
+
+// 	return nil
+// }
